fc1697fe01150e2ad2bcee6ed7e7bf526eb8488f
kakara-apps <kakaraappsrizki@gmail.com>
2021-03-31 20:46:02 +0700

    commit pertama

---
+++ app/src/main/java/com/mrizkimn/profile_rizkimaulana/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mrizkimn.profile_rizkimaulana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
